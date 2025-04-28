594f42e70e78b661333f2ea7561dee28511900af
Mmm44556 <asdshake0708@gmail.com>
2025-04-28 19:53:19 +0800

    fix: force renderded

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -3,6 +3,10 @@ import { del, list, head } from "@vercel/blob";
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getDbUserId } from "@/app/actions/user.action";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export async function POST(request: Request): Promise<NextResponse> {
   const body = (await request.json()) as HandleUploadBody;
 
--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 export default function SelectConversationPage() {
   return (
     <div className="flex basis-full h-full justify-center items-center max-lg:pt-5">
